599fee2273266e7c4901b630112bc60018707c6d
GitHub Action <action@github.com>
2021-06-11 17:31:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/patriciagallardo/qmlplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.patriciagallardo.qmlplugin.services
+
+import com.github.patriciagallardo.qmlplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
